a971a1f37f7f44200af21925e98f789fc64aa3f8
jatin vats <500068636@stu.upes.ac.in>
2021-05-02 13:32:33 +0530

    css

--- function.js
+++ function.js
@@ -107,7 +107,7 @@ class App {
     const newNote = {
       title,
       text,
-      tstamp: new Date().get,
+      tstamp: new Date().toLocaleString(),
       color: "#90ccf4",
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
     };
