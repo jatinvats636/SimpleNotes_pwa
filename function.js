e54d523ae584b2dbf9a8d5c8e69cf62c7f2d2537
jatin vats <500068636@stu.upes.ac.in>
2021-05-02 09:40:37 +0530

    js update

--- function.js
+++ function.js
@@ -171,7 +171,9 @@ class App {
           <div class="${note.title && "note-title"}">${note.title}</div>
           <div class="note-text">${note.text}</div>
           <div class="toolbar-container">
-          <div class="note-date">${note.tstamp}</div>
+          <div class="note-date"><img class="toolbar-delete" src="/images/updated.svg"> ${
+            note.tstamp
+          }</div>
             <div class="toolbar">
               <img data-id=${
                 note.id
